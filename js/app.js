373f2ebdd1dbded3f5d6bbfbf3d713a0a5c72cf7
Frucou <fruicou.sansus3@gmail.com>
2021-11-09 13:27:11 +0100

    Primer commit del ejercicio
    Realizados:
    -Html, estructura básica de la página más el bootstrap
    -Css, básico para añadir un grid y mejor forma a las listas
    -Js, Carga y borra datos y visualización de los datos

---
+++ js/app.js
@@ -0,0 +1,85 @@
+"use strict";
+
+/**
+ * array.push() Carga un elemento en un array
+ */
+
+// De la siguiente forma podemos cargar elementos teniendo el enlace
+// de script en el head, debemos colocar todo dentro de la función
+// para que se ejecute correctamente
+let numeros = [];
+let interruptor = true;
+// console.log(numeros);
+const init = () => {
+    // nodos
+    let form = document.querySelector(".form-floating")
+    let input = document.querySelector("#floatingInputValue");
+    let output = document.querySelector(".salida"); // es la 2ª etiqueta article
+    let btnBorrar = document.querySelector(".btn-secondary");
+    let success = document.querySelector(".alert-success");
+    
+
+
+    let ul = null;
+
+
+    // Cargo el array primero
+    success.innerHTML = `Array[${numeros}]`;
+
+    // Funciones
+    const crearUl = () => {
+        ul = document.createElement("ul");
+        ul.className = "lista";
+        // console.log(ul);
+        output.appendChild(ul);
+        // console.log(output.value);
+    }
+
+    const crearLi = valor => {
+        const li = document.createElement("li");
+        li.innerHTML = valor;
+        ul.appendChild(li);
+    }
+
+    const deleteLi = () => {
+        numeros = [];
+        success.innerHTML = `Array[${numeros}]`;
+        if (ul !== null) {
+            ul.innerHTML = "";
+        }
+    }
+    
+    // Eventos
+    form.addEventListener(
+        "submit",
+        ev => {
+            ev.preventDefault();
+            numeros.push(Number(input.value));
+            // console.log(numeros);
+            console.log(interruptor);
+            
+            if (interruptor) {
+                interruptor = false;
+                console.log(interruptor);
+                crearUl();
+                
+            }
+            crearLi(numeros);
+            success.innerHTML = `Array[${numeros}]`
+        }
+    );
+
+    //  con la siguiente función borraremos los LIs
+        btnBorrar.addEventListener(
+            "click",
+            deleteLi
+        );
+
+}
+
+
+// Inicialización del evento
+window.addEventListener(
+    "load",
+    init
+);
